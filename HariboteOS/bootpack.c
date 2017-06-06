bc565d49b8a22807a37863617f4d078336cd1e7d
56 <y_g0r0@yahoo.co.jp>
2017-06-07 00:24:59 +0900

    C言語からメモリを読む

--- HariboteOS/bootpack.c
+++ HariboteOS/bootpack.c
@@ -1,15 +1,19 @@
 /* 他のファイルで作った関数がありますとCコンパイラに教える */
-
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 /* 関数宣言なのに、{}がなくていきなり;を書くと、他のファイルにあるからよろしくね、という意味になるのです。 */
 
 void HariMain(void)
 {
-fin:
-	/* ここにHLTを入れたいのだが、C言語ではHLTが使えない! */
-	/* これでnaskfunc.nasの_io_hltが実行されます */
-	io_hlt();
-	goto fin;
-  
+ 	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
+	
+	for(i = 0xa0000; i <= 0xaffff; i++) {
+	 	write_mem8(i,15); /* MOV BYTE [i],15 */ 
+	 }
+
+	 for (;;){
+	   io_hlt();
+	 }
+
 }
